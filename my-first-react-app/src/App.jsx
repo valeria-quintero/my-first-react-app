9927f79da9075dcd2fd13eb30513790d72a5ceba
Valeria <melissavlqf@gmail.com>
2026-09-04 17:36:42 -0600

    Implementa la busqueda

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -22,13 +22,15 @@ const App = () => {
     const [isLoading, setIsLoading] = useState(false);
 
 
-    const fetchMovies = async () => {
+    const fetchMovies = async (query = '') => {
 
         setIsLoading(true);
         setErrorMessage('');
 
         try {
-            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const endpoint = query 
+            ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+            : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
             const response = await fetch(endpoint, API_OPTIONS);
 
@@ -54,8 +56,8 @@ const App = () => {
     }
 
     useEffect(() => {
-        fetchMovies();
-    }, []);
+        fetchMovies(searchTerm);
+    }, [searchTerm]);
 
     return (
         <main>
