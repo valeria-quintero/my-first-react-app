9592c9640f2d129deda7bf1a597da786b891c796
Valeria <melissavlqf@gmail.com>
2026-09-04 12:02:54 -0600

    Agrega el useState hook

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,25 +1,26 @@
+import { useState } from "react"
+
 const Card = ({title}) => {
+  const [hasLicked, setHasLicked] = useState(false);
+
   return (
-    <div className="card" style={{
-      border: '1px solid #4b5362',
-      padding: '20px',
-      margin: '10px',
-      backgroundColor: '#31363f',
-      borderRadius: '10px',
-      minHeight: '100px',
-    }}>
+    <div className="card" >
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLicked(!hasLicked)}>
+        {hasLicked ? '💓' : '🤍'}
+      </button>
     </div>
   )
 }
 
 const App = () => {
+
   return (
     <div className="card-container">
-
       <Card title='Star Wars' rating={5} isCool={true} />
       <Card title='Avatar' />
-      <Card title='the lion king' />
+      <Card title='the Lion King' />
     </div>
   )
 }
