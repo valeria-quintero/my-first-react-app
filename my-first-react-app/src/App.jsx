ddc81b982a84fcc47863bd17d41e9b9f9bbdc7c8
Valeria <melissavlqf@gmail.com>
2026-09-04 10:53:58 -0600

    Agrega los archivos del proyecto

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+const Card = () => {
+  return (
+    <h2>Card Component</h2>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h2>Functional Arrow Component</h2>
+
+      <Card />
+    </div>
+  )
+}
+ 
+export default App
