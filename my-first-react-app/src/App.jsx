c53886288a00c674cd81a3a65f155ca88e757ebb
Valeria <melissavlqf@gmail.com>
2026-09-04 14:36:15 -0600

    Realiza encabezados

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (
-        <div>
-            <h1 class="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </div>
+        <main>
+            <div className="pattern" />
+
+            <div className="wrapper"> 
+                <header>
+                    <img src="./hero-img.png" alt="Hero Banner" />
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+                </header>
+                
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                <h1 className='text-white'>{searchTerm}</h1>
+            </div>
+        </main>
     )
 }
 
