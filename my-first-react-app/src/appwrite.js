793b86e4c87209f40f5b8e5e0c585d62d0ffa351
Valeria <melissavlqf@gmail.com>
2026-09-05 10:07:17 -0600

    Muestra las películas en tendencia

--- my-first-react-app/src/appwrite.js
+++ my-first-react-app/src/appwrite.js
@@ -53,3 +53,22 @@ export const updateSearchCount = async (searchTerm, movie) => {
         console.log(error);
     }
 }
+
+export const getTrendingMovies = async () => {
+    try {
+        const result = await tablesDB.listRows({
+            databaseId: DATABASE_ID,
+            tableId: TABLE_ID,
+            queries: [
+                Query.limit(5),
+                Query.orderDesc('count')
+            ]
+        });
+
+        return result.rows;
+
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
